90865bfa2288013940dace7932201d0433721fea
Nico Del Rosario <57257533+Nirioppai@users.noreply.github.com>
2022-09-12 22:45:21 +0800

    project directory set up

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/globals.css';
+
+// can put global elements here and use authentication on this file
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
