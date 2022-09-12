c5d527dde68c557530bada4cdedecf574d6f78f5
Nico Del Rosario <57257533+Nirioppai@users.noreply.github.com>
2022-09-12 23:30:04 +0800

    basic routing

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return <div className={styles.container}>Home</div>;
+  return (
+    <div className={styles.container}>
+      <Link
+        // if true, pag pinindot agad yung link, mag loload yung page
+        prefetch={false}
+        href={{ pathname: '/[username', query: { username: 'jeffd23' } }}
+      ></Link>
+    </div>
+  );
 }
