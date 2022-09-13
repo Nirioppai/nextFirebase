83bf77f8b479e01424aeb3ef64324a5af59f2c97
Nico Del Rosario <57257533+Nirioppai@users.noreply.github.com>
2022-09-13 08:06:32 +0800

    setup auth, downgrade to firebase v8

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,41 @@
+import { auth, googleAuthProvider } from '../lib/firebase';
+
 export default function EnterPage({}) {
+  const user = null;
+  const username = null;
   return (
+    // if user signed out, render <SignInButton/>
+    // if user signed in, but missing username, render <UsernameForm/>
+    // if user signed in, and has username, render <SignOutButton/>
     <main>
-      <h1>Sign Up</h1>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
     </main>
   );
 }
+
+function SignInButton() {
+  // todo: wrap this in try catch block
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleAuthProvider);
+  };
+
+  return (
+    <button className='btn-google' onClick={signInWithGoogle}>
+      <img src='/google.png' alt='Google Logo' /> Sign in with Google
+    </button>
+  );
+}
+
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
+function UsernameForm() {}
